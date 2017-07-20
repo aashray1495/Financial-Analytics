a55745e31b1c22d9e2658d5985834ba1090c72da
aashray1495 <aashray1495@hotmail.com>
2017-07-20 17:19:54 +0530

    Add files via upload
    #Predicting Future stock price using Monte Carlo Simulation(Asset Pricing)

--- predstock.py
+++ predstock.py
@@ -24,8 +24,17 @@ stddev=log_ret.std()
 t_int=1000
 itern=10
 
-daily_ret = np.exp(drift.values + stddev.values * norm.ppf(np.random.rand(t_int, itern)))
+daily_ret = np.exp(drift.values + stddev.values * norm.ppf(np.random.rand(t_int, itern)))#Daily Returns Formula
 
+print('Daily Returns of PG is:', daily_ret)
 
-print('Predicted Daily Return of PG is:', daily_ret)
+S0 = dat.iloc[-1]#Setting 1st price equivalent to the latest price of the asset
+price_list = np.zeros_like(daily_ret)#Creating matrix of 0s and using loop to insert values
+
+for t in range(1, t_int):
+	price_list[t]= price_list[t-1] + daily_ret[t]
+
+plt.figure(figsize=(10,6))
+plt.plot(price_list)
+plt.show()
 
